8665aa7be22319b2c83de6cf9c0998c9eb210fd0
nouxiaxia <nouxiaxia@outlook.com>
2026-04-25 15:15:12 +0800

    新增： 添加员工

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,10 +1,12 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel
 public class EmployeeDTO implements Serializable {
 
     private Long id;
