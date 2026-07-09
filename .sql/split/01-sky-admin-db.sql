8997ebd67dfcac063cac2e9437541521d760e848
huang-sitong <huang-sitong@outlook.com>
2026-07-09 20:19:15 +0800

    fix：修正存储在数据库里的密码为密文

--- .sql/split/01-sky-admin-db.sql
+++ .sql/split/01-sky-admin-db.sql
@@ -25,5 +25,5 @@ CREATE TABLE `employee` (
   UNIQUE KEY `idx_username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin COMMENT='员工信息';
 
--- 初始管理员账号: admin / 123456
-INSERT INTO `employee` VALUES (1,'管理员','admin','123456','13812312312','1','110101199001010047',1,'2022-02-15 15:51:20','2022-02-17 09:16:20',10,1);
+-- 初始管理员账号: admin / 123456 (MD5)
+INSERT INTO `employee` VALUES (1,'管理员','admin','e10adc3949ba59abbe56e057f20f883e','13812312312','1','110101199001010047',1,'2022-02-15 15:51:20','2022-02-17 09:16:20',10,1);
